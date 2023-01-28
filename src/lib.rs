49482361ef9a283165781b2904cf169abf52e937
Lucas Crownover <lcrownover127@gmail.com>
2023-01-28 15:11:32 -0800

    split to lib

---
+++ src/lib.rs
@@ -0,0 +1,167 @@
+use std::collections::HashMap;
+#[derive(Debug)]
+pub struct Ctx {
+    pub noop: bool,
+    pub modify_acls: bool,
+    pub uidmap: HashMap<u32, u32>,
+    pub gidmap: HashMap<u32, u32>,
+}
+
+pub mod files {
+    use crate::Ctx;
+    use file_owner::PathExt;
+    use rayon::prelude::*;
+    use std::fs;
+    use std::fs::{DirEntry, ReadDir};
+    use std::os::linux::fs::MetadataExt;
+    use std::path::Path;
+    pub fn parse_file_listing(files: ReadDir) -> Vec<DirEntry> {
+        let mut outfiles: Vec<DirEntry> = vec![];
+        for file in files {
+            match file {
+                Ok(de) => outfiles.push(de),
+                Err(e) => {
+                    eprintln!("failed to parse file: {e}");
+                    continue;
+                }
+            };
+        }
+        outfiles
+    }
+
+    pub fn run_dir(ctx: &Ctx, path: &impl AsRef<Path>) {
+        let file_listing = match fs::read_dir(path) {
+            Ok(f) => f,
+            Err(e) => {
+                eprintln!(
+                    "failed to stat file: {}, error: {}",
+                    path.as_ref().display(),
+                    e
+                );
+                return;
+            }
+        };
+        let files = parse_file_listing(file_listing);
+        files.par_iter().for_each(move |f| {
+            let ft = match f.file_type() {
+                Ok(ft) => ft,
+                Err(e) => {
+                    eprintln!("failed to parse file type: {e}");
+                    return;
+                }
+            };
+            let fm = match f.metadata() {
+                Ok(fm) => fm,
+                Err(e) => {
+                    eprintln!("failed to parse file metadata: {e}");
+                    return;
+                }
+            };
+            let current_uid = fm.st_uid();
+            match ctx.uidmap.get(&current_uid) {
+                Some(new_uid) => {
+                    println!(
+                        "PREVIEW: changing uid from {current_uid} to {new_uid} for file {}",
+                        f.path().display()
+                    );
+                    if ctx.noop == false {
+                        match f.path().set_owner(*new_uid) {
+                            Ok(_) => (),
+                            Err(e) => eprintln!(
+                                "failed to set uid for file {}, error: {}",
+                                f.path().display(),
+                                e
+                            ),
+                        };
+                        println!(
+                            "CHANGED: current uid {:?}, new uid {}",
+                            f.path().owner().unwrap().id(),
+                            new_uid
+                        );
+                    }
+                }
+                None => (),
+            };
+            let current_gid = fm.st_gid();
+            match ctx.gidmap.get(&current_gid) {
+                Some(new_gid) => {
+                    println!(
+                        "PREVIEW: changing gid from {current_gid} to {new_gid} for file {}",
+                        f.path().display()
+                    );
+                    if ctx.noop == false {
+                        match f.path().set_group(*new_gid) {
+                            Ok(_) => (),
+                            Err(e) => eprintln!(
+                                "failed to set gid for file {}, error: {}",
+                                f.path().display(),
+                                e
+                            ),
+                        };
+                        println!(
+                            "CHANGED: current gid {:?}, new gid {}",
+                            f.path().group().unwrap().id(),
+                            new_gid
+                        );
+                    }
+                }
+                None => (),
+            };
+
+            // need to get acls too but i cant do that on mac
+            if ctx.modify_acls {
+                println!("modifying acls")
+            }
+
+            if ft.is_dir() {
+                run_dir(&ctx, &f.path())
+            }
+        });
+    }
+}
+
+pub mod pairs {
+    use anyhow::{bail, Result};
+    use std::collections::HashMap;
+    #[derive(Debug)]
+    pub struct Idpair {
+        old_id: u32,
+        new_id: u32,
+    }
+    impl Idpair {
+        pub fn from_string(idpair: &str) -> Result<Idpair, anyhow::Error> {
+            let old_id = match idpair.split(':').nth(0) {
+                Some(s) => match s.parse::<u32>() {
+                    Ok(o) => o,
+                    Err(_) => bail!("unable to parse id string '{s}' to int"),
+                },
+                None => bail!("invalid idpair. expected format (old:new) '890:211790'"),
+            };
+            let new_id = match idpair.split(':').nth(1) {
+                Some(s) => match s.parse::<u32>() {
+                    Ok(o) => o,
+                    Err(_) => bail!("unable to parse id string '{s}' to int"),
+                },
+                None => bail!("invalid idpair. expected format (old:new) '890:211790'"),
+            };
+            // let new = idpair.split(":").nth(1)?.parse::<u32>()?;
+            Ok(Idpair { old_id, new_id })
+        }
+    }
+
+    pub fn get_map_from_pairs(pairs: Vec<String>) -> Result<HashMap<u32, u32>, anyhow::Error> {
+        let mut idmap: HashMap<u32, u32> = HashMap::new();
+        for pair in pairs {
+            match Idpair::from_string(&pair) {
+                Ok(u) => match idmap.insert(u.old_id, u.new_id) {
+                    Some(_) => {
+                        bail!("duplicate old id found in provided idpairs. check your input data.")
+                    }
+                    None => (),
+                },
+                Err(e) => bail!(e),
+            }
+        }
+        Ok(idmap)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,11 @@
 use anyhow::{bail, Result};
 use clap::Parser;
-use file_owner::PathExt;
-use rayon::prelude::*;
-use std::collections::HashMap;
-use std::fs::{self, DirEntry, ReadDir};
 use std::io;
-use std::os::macos::fs::MetadataExt;
-use std::path::Path;
 use std::process::exit;
 
-#[derive(Debug)]
-struct Idpair {
-    old_id: u32,
-    new_id: u32,
-}
-impl Idpair {
-    fn from_string(idpair: &str) -> Result<Idpair, anyhow::Error> {
-        let old_id = match idpair.split(':').nth(0) {
-            Some(s) => match s.parse::<u32>() {
-                Ok(o) => o,
-                Err(_) => bail!("unable to parse id string '{s}' to int"),
-            },
-            None => bail!("invalid idpair. expected format (old:new) '890:211790'"),
-        };
-        let new_id = match idpair.split(':').nth(1) {
-            Some(s) => match s.parse::<u32>() {
-                Ok(o) => o,
-                Err(_) => bail!("unable to parse id string '{s}' to int"),
-            },
-            None => bail!("invalid idpair. expected format (old:new) '890:211790'"),
-        };
-        // let new = idpair.split(":").nth(1)?.parse::<u32>()?;
-        Ok(Idpair { old_id, new_id })
-    }
-}
-
-fn get_map_from_pairs(pairs: Vec<String>) -> Result<HashMap<u32, u32>, anyhow::Error> {
-    let mut idmap: HashMap<u32, u32> = HashMap::new();
-    for pair in pairs {
-        match Idpair::from_string(&pair) {
-            Ok(u) => match idmap.insert(u.old_id, u.new_id) {
-                Some(_) => {
-                    bail!("duplicate old id found in provided idpairs. check your input data.")
-                }
-                None => (),
-            },
-            Err(e) => bail!(e),
-        }
-    }
-    Ok(idmap)
-}
-
-#[derive(Debug)]
-struct Ctx {
-    noop: bool,
-    modify_acls: bool,
-    uidmap: HashMap<u32, u32>,
-    gidmap: HashMap<u32, u32>,
-}
+use chowner_rs::files;
+use chowner_rs::pairs;
+use chowner_rs::Ctx;
 
 /// Blazingly fast filesystem modifier
 #[derive(Parser)]
@@ -87,102 +35,6 @@ struct Cli {
     noop: bool,
 }
 
-fn parse_file_listing(files: ReadDir) -> Vec<DirEntry> {
-    let mut outfiles: Vec<DirEntry> = vec![];
-    for file in files {
-        match file {
-            Ok(de) => outfiles.push(de),
-            Err(e) => {
-                eprintln!("failed to parse file: {e}");
-                continue;
-            }
-        };
-    }
-    outfiles
-}
-
-fn run_dir(ctx: &Ctx, path: &impl AsRef<Path>) {
-    let file_listing = match fs::read_dir(path) {
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!(
-                "failed to stat file: {}, error: {}",
-                path.as_ref().display(),
-                e
-            );
-            return;
-        }
-    };
-    let files = parse_file_listing(file_listing);
-    files.par_iter().for_each(move |f| {
-        let ft = match f.file_type() {
-            Ok(ft) => ft,
-            Err(e) => {
-                eprintln!("failed to parse file type: {e}");
-                return;
-            }
-        };
-        let fm = match f.metadata() {
-            Ok(fm) => fm,
-            Err(e) => {
-                eprintln!("failed to parse file metadata: {e}");
-                return;
-            }
-        };
-        let current_uid = fm.st_uid();
-        match ctx.uidmap.get(&current_uid) {
-            Some(new_uid) => {
-                println!(
-                    "PREVIEW: changing uid from {current_uid} to {new_uid} for file {}",
-                    f.path().display()
-                );
-                if ctx.noop == false {
-                    match f.path().set_owner(*new_uid) {
-                        Ok(_) => (),
-                        Err(e) => eprintln!("failed to set uid for file {}, error: {}", f.path().display(), e)
-                    };
-                    println!(
-                        "CHANGED: current uid {:?}, new uid {}",
-                        f.path().owner().unwrap().id(),
-                        new_uid
-                    );
-                }
-            }
-            None => (),
-        };
-        let current_gid = fm.st_gid();
-        match ctx.gidmap.get(&current_gid) {
-            Some(new_gid) => {
-                println!(
-                    "PREVIEW: changing gid from {current_gid} to {new_gid} for file {}",
-                    f.path().display()
-                );
-                if ctx.noop == false {
-                    match f.path().set_group(*new_gid) {
-                        Ok(_) => (),
-                        Err(e) => eprintln!("failed to set gid for file {}, error: {}", f.path().display(), e)
-                    };
-                    println!(
-                        "CHANGED: current gid {:?}, new gid {}",
-                        f.path().group().unwrap().id(),
-                        new_gid
-                    );
-                }
-            }
-            None => (),
-        };
-
-        // need to get acls too but i cant do that on mac
-        if ctx.modify_acls {
-            println!("modifying acls")
-        }
-
-        if ft.is_dir() {
-            run_dir(&ctx, &f.path())
-        }
-    });
-}
-
 fn main() -> Result<()> {
     let args = Cli::parse();
     if args.threads > 0 {
@@ -190,11 +42,11 @@ fn main() -> Result<()> {
             .num_threads(args.threads)
             .build_global()?;
     }
-    let uidmap = match get_map_from_pairs(args.uidpair) {
+    let uidmap = match pairs::get_map_from_pairs(args.uidpair) {
         Ok(m) => m,
         Err(e) => bail!(e),
     };
-    let gidmap = match get_map_from_pairs(args.gidpair) {
+    let gidmap = match pairs::get_map_from_pairs(args.gidpair) {
         Ok(m) => m,
         Err(e) => bail!(e),
     };
@@ -220,7 +72,7 @@ fn main() -> Result<()> {
     }
 
     // here we go
-    run_dir(&ctx, &args.path);
+    files::run_dir(&ctx, &args.path);
 
     Ok(())
 }
