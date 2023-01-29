c9e73a434e03f290cb3b978a0d315f018e9e4acc
Lucas Crownover <lcrownover127@gmail.com>
2023-01-28 18:01:16 -0800

    dev

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub mod acl {
     use anyhow::{bail, Result};
     use posix_acl::{PosixACL, Qualifier};
     use std::path::Path;
+
     pub fn update_access_acl(ctx: &Ctx, path: &impl AsRef<Path>) -> Result<(), anyhow::Error> {
         println!("Scanning Access ACLs on path: {:?}", path.as_ref());
         let mut acl = match PosixACL::read_acl(path) {
@@ -26,6 +27,10 @@ pub mod acl {
                     match new_uid {
                         Some(new_uid) => {
                             println!("Uid {uid} found in ACL, replacing with uid {new_uid}");
+                            if ctx.noop {
+                                println!("noop, not making changes");
+                                continue;
+                            }
                             println!("Removing Access ACL for old uid: {uid}");
                             acl.remove(Qualifier::User(uid));
                             println!("Adding Access ACL for new uid: {new_uid}");
@@ -39,6 +44,10 @@ pub mod acl {
                     match new_gid {
                         Some(new_gid) => {
                             println!("Gid {gid} found in ACL, replacing with gid {new_gid}");
+                            if ctx.noop {
+                                println!("noop, not making changes");
+                                continue;
+                            }
                             println!("Removing Access ACL for old gid: {gid}");
                             acl.remove(Qualifier::Group(gid));
                             println!("Adding Access ACL for new gid: {new_gid}");
@@ -74,6 +83,10 @@ pub mod acl {
                     match new_uid {
                         Some(new_uid) => {
                             println!("Uid {uid} found in ACL, replacing with uid {new_uid}");
+                            if ctx.noop {
+                                println!("noop, not making changes");
+                                continue;
+                            }
                             println!("Removing Default ACL for old uid: {uid}");
                             acl.remove(Qualifier::User(uid));
                             println!("Adding Default ACL for new uid: {new_uid}");
@@ -87,6 +100,10 @@ pub mod acl {
                     match new_gid {
                         Some(new_gid) => {
                             println!("Gid {gid} found in ACL, replacing with gid {new_gid}");
+                            if ctx.noop {
+                                println!("noop, not making changes");
+                                continue;
+                            }
                             println!("Removing Default ACL for old gid: {gid}");
                             acl.remove(Qualifier::Group(gid));
                             println!("Adding Default ACL for new gid: {new_gid}");
@@ -99,9 +116,13 @@ pub mod acl {
             }
         }
         println!("Writing changes to Default ACL");
-        match acl.write_acl(path) {
-            Ok(_) => println!("Successfully wrote changes to Default ACL"),
-            Err(e) => bail!("Failed to write acl: {e}"),
+        if !ctx.noop {
+            match acl.write_acl(path) {
+                Ok(_) => println!("Successfully wrote changes to Default ACL"),
+                Err(e) => bail!("Failed to write acl: {e}"),
+            }
+        } else {
+            println!("noop, not making changes");
         }
         Ok(())
     }
@@ -110,12 +131,15 @@ pub mod acl {
 pub mod files {
     use crate::acl;
     use crate::Ctx;
+    use anyhow::{bail, Result};
     use file_owner::PathExt;
     use rayon::prelude::*;
     use std::fs;
+    use std::fs::Metadata;
     use std::fs::{DirEntry, ReadDir};
     use std::os::linux::fs::MetadataExt;
     use std::path::Path;
+
     pub fn parse_file_listing(files: ReadDir) -> Vec<DirEntry> {
         let mut outfiles: Vec<DirEntry> = vec![];
         for file in files {
@@ -130,105 +154,138 @@ pub mod files {
         outfiles
     }
 
-    pub fn run_dir(ctx: &Ctx, path: &impl AsRef<Path>) {
+    fn get_file_metadata(f: &DirEntry) -> Result<Metadata, anyhow::Error> {
+        let fm = match f.metadata() {
+            Ok(fm) => fm,
+            Err(e) => {
+                bail!("failed to parse file metadata: {e}");
+            }
+        };
+        Ok(fm)
+    }
+
+    fn get_file_listing(path: &impl AsRef<Path>) -> Result<ReadDir, anyhow::Error> {
         let file_listing = match fs::read_dir(path) {
             Ok(f) => f,
             Err(e) => {
-                eprintln!(
+                bail!(
                     "failed to stat file: {}, error: {}",
                     path.as_ref().display(),
                     e
                 );
-                return;
             }
         };
-        let files = parse_file_listing(file_listing);
-        files.par_iter().for_each(move |f| {
-            let ft = match f.file_type() {
-                Ok(ft) => ft,
-                Err(e) => {
-                    eprintln!("failed to parse file type: {e}");
-                    return;
-                }
-            };
-            if ft.is_symlink() {
-                return;
-            }
-            let fm = match f.metadata() {
-                Ok(fm) => fm,
-                Err(e) => {
-                    eprintln!("failed to parse file metadata: {e}");
-                    return;
-                }
-            };
-            let current_uid = fm.st_uid();
-            match ctx.uidmap.get(&current_uid) {
-                Some(new_uid) => {
-                    println!(
-                        "PREVIEW: changing uid from {current_uid} to {new_uid} for file {}",
-                        f.path().display()
-                    );
-                    if ctx.noop == false {
-                        match f.path().set_owner(*new_uid) {
-                            Ok(_) => (),
-                            Err(e) => eprintln!(
-                                "failed to set uid for file {}, error: {}",
-                                f.path().display(),
-                                e
-                            ),
-                        };
-                        println!(
-                            "CHANGED: current uid {:?}, new uid {}",
-                            f.path().owner().unwrap().id(),
-                            new_uid
-                        );
-                    }
-                }
-                None => (),
-            };
-            let current_gid = fm.st_gid();
-            match ctx.gidmap.get(&current_gid) {
-                Some(new_gid) => {
-                    println!(
-                        "PREVIEW: changing gid from {current_gid} to {new_gid} for file {}",
-                        f.path().display()
-                    );
-                    if ctx.noop == false {
-                        match f.path().set_group(*new_gid) {
-                            Ok(_) => (),
-                            Err(e) => eprintln!(
-                                "failed to set gid for file {}, error: {}",
-                                f.path().display(),
-                                e
-                            ),
-                        };
-                        println!(
-                            "CHANGED: current gid {:?}, new gid {}",
-                            f.path().group().unwrap().id(),
-                            new_gid
-                        );
-                    }
-                }
-                None => (),
-            };
+        Ok(file_listing)
+    }
 
-            if ctx.modify_acls {
-                match acl::update_access_acl(&ctx, &f.path()) {
-                    Ok(_) => (),
-                    Err(e) => eprintln!("{e}"),
-                };
-                if ft.is_dir() {
-                    match acl::update_default_acl(&ctx, &f.path()) {
+    fn change_uid(ctx: &Ctx, f: &DirEntry) -> Result<(), anyhow::Error> {
+        let fm = get_file_metadata(f)?;
+        let current_uid = fm.st_uid();
+        match ctx.uidmap.get(&current_uid) {
+            Some(new_uid) => {
+                println!(
+                    "PREVIEW: changing uid from {current_uid} to {new_uid} for file {}",
+                    f.path().display()
+                );
+                if ctx.noop == false {
+                    match f.path().set_owner(*new_uid) {
                         Ok(_) => (),
-                        Err(e) => eprintln!("{e}"),
+                        Err(e) => eprintln!(
+                            "failed to set uid for file {}, error: {}",
+                            f.path().display(),
+                            e
+                        ),
                     };
+                    println!(
+                        "CHANGED: current uid {:?}, new uid {}",
+                        f.path().owner().unwrap().id(),
+                        new_uid
+                    );
                 }
             }
+            None => (),
+        };
+        Ok(())
+    }
 
-            if ft.is_dir() {
-                run_dir(&ctx, &f.path())
+    fn change_gid(ctx: &Ctx, f: &DirEntry) -> Result<(), anyhow::Error> {
+        let fm = get_file_metadata(f)?;
+        let current_gid = fm.st_gid();
+        match ctx.gidmap.get(&current_gid) {
+            Some(new_gid) => {
+                println!(
+                    "PREVIEW: changing gid from {current_gid} to {new_gid} for file {}",
+                    f.path().display()
+                );
+                if ctx.noop == false {
+                    match f.path().set_group(*new_gid) {
+                        Ok(_) => (),
+                        Err(e) => eprintln!(
+                            "failed to set gid for file {}, error: {}",
+                            f.path().display(),
+                            e
+                        ),
+                    };
+                    println!(
+                        "CHANGED: current gid {:?}, new gid {}",
+                        f.path().group().unwrap().id(),
+                        new_gid
+                    );
+                }
             }
+            None => (),
+        };
+        Ok(())
+    }
+
+    fn process_dir_entry(ctx: &Ctx, f: &DirEntry) -> Result<(), anyhow::Error> {
+        let ft = match f.file_type() {
+            Ok(ft) => ft,
+            Err(e) => {
+                bail!("failed to parse file type: {e}");
+            }
+        };
+
+        // Skip symlinks, they're nuthin but trouble
+        if ft.is_symlink() {
+            return Ok(())
+        }
+
+        // Run the logic to check and swap old uid with new uid
+        change_uid(&ctx, &f)?;
+        change_gid(&ctx, &f)?;
+
+        // Modify the posix ACLs if flag was provided
+        if ctx.modify_acls {
+            acl::update_access_acl(&ctx, &f.path())?;
+            if ft.is_dir() {
+                acl::update_default_acl(&ctx, &f.path())?;
+            }
+        }
+
+        // If the item is a dir, do it all again!
+        if ft.is_dir() {
+            run_dir(&ctx, &f.path())?;
+        }
+
+        Ok(())
+    }
+
+    pub fn run_dir(ctx: &Ctx, path: &impl AsRef<Path>) -> Result<(), anyhow::Error> {
+        // do the stuff to the provided Path
+
+        // then list all its children and do the stuff
+        let file_listing = get_file_listing(&path)?;
+        let files = parse_file_listing(file_listing);
+
+        files.par_iter().for_each(move |f| {
+            match process_dir_entry(&ctx, &f) {
+                Ok(_) => (),
+                Err(e) => eprintln!("{e}"),
+            };
         });
+
+        Ok(())
     }
 }
 
