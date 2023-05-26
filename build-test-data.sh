1330693e0a5af6416ee0579dd87e173501a593b1
Lucas Crownover <lcrownover127@gmail.com>
2023-05-26 14:30:53 -0700

    fix data

--- build-test-data.sh
+++ build-test-data.sh
@@ -13,44 +13,44 @@ rm -rf /opt/testdata
 
 # --uidpair 84:87 --uidpair 85:88 --uidpair 86:89
 
-sudo useradd one
-sudo useradd oneuser
-sudo useradd twouser
-sudo useradd threeuser
-sudo useradd oneusernew
-sudo useradd twousernew
-sudo useradd threeusernew
+useradd one
+useradd oneuser
+useradd twouser
+useradd threeuser
+useradd oneusernew
+useradd twousernew
+useradd threeusernew
 
-sudo mkdir -p $TD
-sudo mkdir -p $TD/shared/
-sudo chown oneuser $TD/shared/
+mkdir -p $TD
+mkdir -p $TD/shared/
+chown oneuser $TD/shared/
 
-sudo mkdir -p $TD/one
-echo "one data" | sudo tee -a $TD/one/onefile
-echo "one shared data" | sudo tee -a $TD/shared/one
-sudo chown -R oneuser $TD/one/
-sudo chmod -R u=rwx,g=rwx $TD/one/
-sudo chown oneuser $TD/shared/one
-sudo setfacl -m u:oneuser:r $TD/shared/
-sudo setfacl -d -m u:oneuser:r $TD/shared/
+mkdir -p $TD/one
+echo "one data" | tee -a $TD/one/onefile
+echo "one shared data" | tee -a $TD/shared/one
+chown -R oneuser $TD/one/
+chmod -R u=rwx,g=rwx $TD/one/
+chown oneuser $TD/shared/one
+setfacl -m u:oneuser:r $TD/shared/
+setfacl -d -m u:oneuser:r $TD/shared/
 
-sudo mkdir -p $TD/two
-echo "two data" | sudo tee -a $TD/two/twofile
-echo "two shared data" | sudo tee -a $TD/shared/two
-sudo chown -R twouser $TD/two/
-sudo chmod -R u=rwx,g=rwx $TD/two/
-sudo chown twouser $TD/shared/two
-sudo setfacl -m u:twouser:r $TD/shared/
-sudo setfacl -d -m u:twouser:r $TD/shared/
+mkdir -p $TD/two
+echo "two data" | tee -a $TD/two/twofile
+echo "two shared data" | tee -a $TD/shared/two
+chown -R twouser $TD/two/
+chmod -R u=rwx,g=rwx $TD/two/
+chown twouser $TD/shared/two
+setfacl -m u:twouser:r $TD/shared/
+setfacl -d -m u:twouser:r $TD/shared/
 
-sudo mkdir -p $TD/three
-echo "three data" | sudo tee -a $TD/three/threefile
-echo "three shared data" | sudo tee -a $TD/shared/three
-sudo chown -R threeuser $TD/three/
-sudo chmod -R u=rwx,g=rwx $TD/three/
-sudo chown threeuser $TD/shared/three
-sudo setfacl -m u:threeuser:r $TD/shared/
-sudo setfacl -d -m u:threeuser:r $TD/shared/
+mkdir -p $TD/three
+echo "three data" | tee -a $TD/three/threefile
+echo "three shared data" | tee -a $TD/shared/three
+chown -R threeuser $TD/three/
+chmod -R u=rwx,g=rwx $TD/three/
+chown threeuser $TD/shared/three
+setfacl -m u:threeuser:r $TD/shared/
+setfacl -d -m u:threeuser:r $TD/shared/
 
 rm -f $TD/one/shared-one
-sudo ln -s $TD/shared/one $TD/one/shared-one
+ln -s $TD/shared/one $TD/one/shared-one
